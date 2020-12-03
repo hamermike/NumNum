a5dce43e28a51e43ca43fb5ed37b28f687e32ce1
Mike Hamer <hamer.mike@gmail.com>
2020-12-03 01:42:04 -0500

    Add Wendy's menu items

--- Program.cs
+++ Program.cs
@@ -10,49 +10,70 @@ namespace NumNum
         {
             var menuItems = new List<MenuItem>()
             {
-                // Sandwiches & Meals
-                new MenuItem { Name = "McRib", Price = 3.69m },
-                // new MenuItem { Name = "McRib Meal", Price = 5.59m },
-                new MenuItem { Name = "Big Mac", Price = 4.91m },
-                // new MenuItem { Name = "Big Mac Meal", Price = 7.08m },
-                new MenuItem { Name = "Bacon Quarter Pounder with Cheese", Price = 5.39m },
-                // new MenuItem { Name = "Bacon Quarter Pounder with Cheese Meal", Price = 7.79m },
-                new MenuItem { Name = "Quarter Pounder with Cheese", Price = 4.91m },
-                // new MenuItem { Name = "Quarter Pounder with Cheese Meal", Price = 7.08m },
+                // Hamburgers
+                new MenuItem { Name = "Pretzel Bacon Pub Cheeseburger", Price = 5.59m },
+                new MenuItem { Name = "Pretzel Bacon Pub Double", Price = 6.59m },
+                new MenuItem { Name = "Pretzel Bacon Pub Triple", Price = 7.59m },
 
-                new MenuItem { Name = "Cheeseburger", Price = 1.19m },
-                new MenuItem { Name = "McDouble", Price = 1.99m },
-                new MenuItem { Name = "Bacon McDouble", Price = 2.00m },
+                new MenuItem { Name = "Jr. Bacon Cheeseburger", Price = 1.99m },
+                new MenuItem { Name = "Jr. Cheeseburger Deluxe", Price = 1.89m },
+                new MenuItem { Name = "Jr. Cheeseburger", Price = 1.79m },
+                new MenuItem { Name = "Double Stack", Price = 2.49m },
+                new MenuItem { Name = "Jr. Hamburger", Price = 1.39m },
 
-                new MenuItem { Name = "McChicken", Price = 1.00m },
-                new MenuItem { Name = "4 Piece McNuggets", Price = 1.79m },
-                new MenuItem { Name = "6 Piece McNuggets", Price = 2.39m },
-                new MenuItem { Name = "10 Piece McNuggets", Price = 4.09m },
-                new MenuItem { Name = "20 Piece McNuggets", Price = 5.99m },
+                // Fries & Sides
+                new MenuItem { Name = "Jr. Natural-Cut Fries", Price = 1.29m },
+                new MenuItem { Name = "Small Natural-Cut Fries", Price = 1.89m },
+                new MenuItem { Name = "Medium Natural-Cut Fries", Price = 2.29m },
+                new MenuItem { Name = "Large Natural-Cut Fries", Price = 2.69m },
 
-                // Cookies
-                new MenuItem { Name = "1 Cookie", Price = 0.69m },
-                new MenuItem { Name = "2 Cookies", Price = 1.29m },
-                new MenuItem { Name = "3 Pack of Cookies", Price = 1.80m },
-                // new MenuItem { Name = "13 Cookie Tote", Price = 5.49m },
+                // Frosty
+                new MenuItem { Name = "Jr. Classic Chocolate Frosty", Price = 1.19m },
+                new MenuItem { Name = "Small Classic Chocolate Frosty", Price = 1.69m },
+                new MenuItem { Name = "Medium Classic Chocolate Frosty", Price = 1.99m },
+                new MenuItem { Name = "Large Classic Chocolate Frosty", Price = 2.39m },
 
-                // Shakes
-
-                // McFlurry
-                new MenuItem { Name = "Regular Chips Ahoy! McFlurry", Price = 2.99m },
-
-                // Pies
-                new MenuItem { Name = "Apple Pie", Price = 1.19m },
-
-                // French Fries
-                new MenuItem { Name = "Small French Fries", Price = 1.59m },
-                new MenuItem { Name = "Medium French Fries", Price = 2.19m },
-                new MenuItem { Name = "Large French Fries", Price = 2.59m },
-
-                // new MenuItem { Name = "Cream Cheese", Price = 0.55m },
-
-                // new MenuItem { Name = "Apple Slices", Price = 0.79m },
+                // Chicken Sandwiches & Nuggets
+                new MenuItem { Name = "Family Size Nuggets", Price = 9.99m },
             };
+
+
+            // var menuItems = new List<MenuItem>()
+            // {
+            //     // Sandwiches & Meals
+            //     new MenuItem { Name = "McRib", Price = 3.69m },
+            //     new MenuItem { Name = "Big Mac", Price = 4.91m },
+            //     new MenuItem { Name = "Bacon Quarter Pounder with Cheese", Price = 5.39m },
+            //     new MenuItem { Name = "Quarter Pounder with Cheese", Price = 4.91m },
+
+            //     new MenuItem { Name = "Cheeseburger", Price = 1.19m },
+            //     new MenuItem { Name = "McDouble", Price = 1.99m },
+            //     new MenuItem { Name = "Bacon McDouble", Price = 2.00m },
+
+            //     new MenuItem { Name = "McChicken", Price = 1.00m },
+            //     new MenuItem { Name = "4 Piece McNuggets", Price = 1.79m },
+            //     new MenuItem { Name = "6 Piece McNuggets", Price = 2.39m },
+            //     new MenuItem { Name = "10 Piece McNuggets", Price = 4.09m },
+            //     new MenuItem { Name = "20 Piece McNuggets", Price = 5.99m },
+
+            //     // Cookies
+            //     new MenuItem { Name = "1 Cookie", Price = 0.69m },
+            //     new MenuItem { Name = "2 Cookies", Price = 1.29m },
+            //     new MenuItem { Name = "3 Pack of Cookies", Price = 1.80m },
+
+            //     // Shakes
+
+            //     // McFlurry
+            //     new MenuItem { Name = "Regular Chips Ahoy! McFlurry", Price = 2.99m },
+
+            //     // Pies
+            //     new MenuItem { Name = "Apple Pie", Price = 1.19m },
+
+            //     // French Fries
+            //     new MenuItem { Name = "Small French Fries", Price = 1.59m },
+            //     new MenuItem { Name = "Medium French Fries", Price = 2.19m },
+            //     new MenuItem { Name = "Large French Fries", Price = 2.59m },
+            // };
 
             var total = (decimal)18.66/*19.24*/;
         
